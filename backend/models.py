5ab0e6e6d6e60733474163deb86600a26a7dabb9
Ata Kocabas <atakocabas@gmail.com>
2021-12-25 00:07:16 +0300

    first

--- backend/models.py
+++ backend/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.db.models.deletion import CASCADE
 
 # Create your models here.
 class User(models.Model):
@@ -16,20 +17,33 @@ class Pet(models.Model):
     age = models.IntegerField(default = 0)
 
     def __str__(self):
-        return self.name, self.type
-
-class VaccinationCard(models.Model):
-    pet = models.ForeignKey(Pet, on_delete=models.CASCADE)
-    vaccination = models.CharField(max_length=100)
-    count = models.IntegerField(default=0)
-
-    def __str__(self):
-        return self.id, Pet.name
+        return self.name + self.type
 
 class Vet(models.Model):
     name = models.CharField(max_length=100)
+    patient = models.CharField(Pet.name)
 
+
+    def __str__(self):
+        return self.name
+
+class Vaccination(models.Model):
+    name = models.CharField(max_length=100)
+    date = models.DateField('Date of Vaccination')
+
+    def __str__(self):
+        return self.name + self.date
+
+class VaccinationCard(models.Model):
+    patient = models.ForeignKey(Pet, on_delete=CASCADE)
+    vaccination = models.ForeignKey(Vaccination, on_delete=CASCADE)
 
     def __str__(self):
         return self.id
 
+class Company(models.Model):
+    user = models.ForeignKey(User, on_delete=CASCADE)
+    name = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.name
