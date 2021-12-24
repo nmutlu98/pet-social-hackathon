883ba9d9b694831d706aa5cacdacb71ab42359f1
Ata Kocabas <atakocabas@gmail.com>
2021-12-24 23:33:38 +0300

    first

--- backend/models.py
+++ backend/models.py
@@ -7,3 +7,29 @@ class User(models.Model):
 
     def __str__(self):
         return self.email
+
+class Pet(models.Model):
+    owner = models.ForeignKey(User, on_delete=models.CASCADE)
+    name = models.CharField(max_length=100)
+    type = models.CharField(max_length=100)
+    breed = models.CharField(max_length=100)
+    age = models.IntegerField(default = 0)
+
+    def __str__(self):
+        return self.name, self.type
+
+class VaccinationCard(models.Model):
+    pet = models.ForeignKey(Pet, on_delete=models.CASCADE)
+    vaccination = models.CharField(max_length=100)
+    count = models.IntegerField(default=0)
+
+    def __str__(self):
+        return self.id, Pet.name
+
+class Vet(models.Model):
+    name = models.CharField(max_length=100)
+
+
+    def __str__(self):
+        return self.id
+
